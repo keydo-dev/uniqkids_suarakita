ffde8dc0f0ee24e17ea84833caff47130d461d66
keydo-dev <syafrilkeydo2009@gmail.com>
2025-10-10 14:31:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/uniqkids_suarakita/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uniqkids_suarakita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:uniqkids_suarakita/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
